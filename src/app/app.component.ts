81a9c70b3506dd56951c66fc3707801184dcd9eb
rayhancse15git <abu@baiust.edu.bd>
2021-01-05 01:11:49 +0600

    data hokup from database done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'client';
+  genders : any;
+
+  constructor (private http: HttpClient){ }
+  
+  ngOnInit() {
+    
+    this.getGenders();
+
+  } 
+  getGenders(){
+    this.http.get('https://localhost:44399/api/MasterData/GetAllGender').subscribe(res=> {
+      this.genders = res;
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
 }
